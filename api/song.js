65fc950640ab57610191ec6f73cd95d1274e7866
Jacky Tung <jackytung20@gmail.com>
2023-07-13 16:25:02 -0400

    Fetch All and By Id API endpoints

--- api/song.js
+++ api/song.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Song = require("../db/models");
+const {Song} = require("../db/models");
 
 // https://developer.spotify.com/documentation/web-api/tutorials/implicit-flow
 
@@ -12,14 +12,28 @@ router.get("/:id/info", async (req, res) => {
       `https://api.spotify.com/v1/tracks/${songId}`,
       {
         method: "GET",
-        // headers: {
-        //     'Authorization': Bearer '' // insert access token here
-        // }
+        headers: {
+            'Authorization': Bearer `${req.params.accessToken}` // insert access token here
+        }
       }
     ).json();
     // songInfo ? res.status(200);
   } catch {
     next(err);
+  }
+});
+
+/**
+ * Fetches all songs
+ */
+router.get("/", async (req, res, next) => {
+  try {
+    const allSongs = await Song.findAll();
+    allSongs
+      ? res.status(200).json(allSongs)
+      : res.status(404).send("Song Listing Not Found");
+  } catch (error) {
+    next(error);
   }
 });
 
