59126a32879bcd15bfe28b3b9765bcdfa1ebd3ef
Rasul Nekzad <nekzadrasul@gmail.com>
2023-07-14 17:38:13 -0400

    fix db url

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize(
-  "postgres://liuke:220701528@98.116.214.14:5432/ttpteam5backend1",
+  "postgres://liuke:220701528@98.116.214.14:5432/ttpteam5backend",
   {
     logging: false,
     dialectModule: require("pg"),
