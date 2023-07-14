68be5b7af6dde8bd26ae4a908e6a338e8fc2c397
Rasul Nekzad <nekzadrasul@gmail.com>
2023-07-14 17:56:24 -0400

    hide url

--- db/db.js
+++ db/db.js
@@ -1,13 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize(
-  "postgres://liuke:220701528@98.116.214.14:5432/ttpteam5backend",
-  {
-    logging: false,
-    dialectModule: require("pg"),
-  }
-);
+const db = new Sequelize(POSTGRES_URL, {
+  logging: false,
+  dialectModule: require("pg"),
+});
 
 // console.log(process.env.POSTGRES_URL);
 
