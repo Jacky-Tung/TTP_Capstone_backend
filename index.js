54e872114e202b2998ff2c35c0f7a141771d31fb
Kevin Liu <you@example.com>
2023-07-17 14:13:31 -0400

    fix: minor bugs in seeding and database connections

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const db = require("./db");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const app = express();
 
 // Mount on API
+app.use(cors()); 
 app.use("/api", require("./api"));
 app.use(bodyParser.json());
 
 // const syncDB = () => db.sync({force: true});
-const syncDB = () => db.sync({ force: true });
+const syncDB = () => db.sync();
 
 const serverRun = () => {
   app.listen(process.env.PORT, () => {
